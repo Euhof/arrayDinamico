2f2bf422705a1aacf26a2e98de58d4620e76b096
Euhof <corvobubonico@gmail.com>
2026-03-26 16:45:55 -0300

    Update arrayDinamico.c

--- arrayDinamico.c
+++ arrayDinamico.c
@@ -8,29 +8,30 @@ typedef struct {
     size_t contagem;
 }Elementos;
 
-// Array dinamico como macro
-#define push_array(xs, x)\
-    do {\
-        if (xs.contagem >= xs.capacidade) {\
-            if (xs.capacidade == 0) {\
-                xs.capacidade = 256;\
-            }else{\
-                xs.capacidade *= 2;\
-            }\
-            xs.item = realloc(xs.item, xs.capacidade * sizeof(xs.item));\
-        }\
-        xs.item[xs.contagem++] = x;\
-    }while(0)
- 
+
+#define push_array(xs, x)                                                   \
+    do {                                                                    \
+        if (xs.contagem >= xs.capacidade) {                                 \
+            if (xs.capacidade == 0) {                                       \
+                xs.capacidade = 256;                                        \
+            }else {                                                         \
+                xs.capacidade *= 2;                                         \
+            }                                                               \
+            xs.item = realloc(xs.item, xs.capacidade * sizeof(*xs.item));   \
+        }                                                                   \
+        xs.item[xs.contagem++] = x;                                         \
+    }while(0)                                                               \
+                                                                            
+
 int main() {
-    Elementos xs = {0}; //xs pode ser lido como "Diversos"
+    Elementos xs = {0};
 
     for (int x = 0; x < 10; x++) {
-        push_array(xs,x); //usando o array dinamico
+        push_array(xs, x);
     }
 
     for (size_t i = 0; i < xs.contagem; i++) {
-        printf("\n->%d", xs.item[i]); //items
+        printf("\n->%d", xs.item[i]);
     }
 
     return 0;
